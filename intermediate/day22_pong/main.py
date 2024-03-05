58c8fec4807131ad4ff1e3029a5386ef16fadb7b
Nick Hofmeister <5778927+niscient-green@users.noreply.github.com>
2024-03-05 15:37:36 -0500

    Refactor midline to new file, tweak padding

--- intermediate/day22_pong/main.py
+++ intermediate/day22_pong/main.py
@@ -1,6 +1,7 @@
 # Recreate the game of Pong
 # Import packages
-from turtle import Turtle, Screen
+from turtle import Screen
+from midline import draw_midline
 from paddle import Paddle
 from ball import Ball
 from scoreboard import Scoreboard
@@ -25,23 +26,6 @@ player1 = Paddle(g.PLAYER1)
 player2 = Paddle(g.PLAYER2)
 ball = Ball()
 scoreboard = Scoreboard()
-
-
-def draw_midline():
-    midline = Turtle()
-    midline.hideturtle()
-    midline.pencolor(g.SCREEN_FG)
-    midline.penup()
-    midline.goto(x=0, y=g.SCREEN_HEIGHT / 2)
-    midline.pendown()
-    midline.setheading(g.SOUTH)
-    num_midline = int(g.SCREEN_HEIGHT / (g.MIDLINE_LEN + g.MIDLINE_SPACE)) + 1
-    for _ in range(0, num_midline):
-        midline.forward(g.MIDLINE_LEN)
-        midline.penup()
-        midline.forward(g.MIDLINE_SPACE)
-        midline.pendown()
-
 
 # Set up screen listening, movement
 # Keys are set up for Niscient Colemak layout
