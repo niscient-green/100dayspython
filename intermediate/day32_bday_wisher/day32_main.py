abeb29676acb37bdae5f8c593aaef5d896b071a0
Nick Hofmeister <5778927+niscient-green@users.noreply.github.com>
2024-03-13 10:12:23 -0400

    Add motivational email mini-project

--- intermediate/day32_bday_wisher/day32_main.py
+++ intermediate/day32_bday_wisher/day32_main.py
@@ -1,2 +1,26 @@
 # Send birthday wishes email
 
+# Import packages
+import smtplib
+import datetime as dt
+
+# # Set constants, globals
+# my_email = "testhofmeister@gmail.com"
+# my_password = "rntqyjnhuxvsootu"
+#
+# # Open email connection, send test email
+# with smtplib.SMTP("smtp.gmail.com", port=587) as connection
+#     connection.starttls()
+#     connection.login(user=my_email, password=my_password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="nick@niscient.com",
+#         msg="Subject: Hi!"
+#             "\n\nJust wishing you a good day.")
+
+now_dtm = dt.datetime.now()
+year_int = now_dtm.year
+month_int = now_dtm.month
+week_day_str = now_dtm.weekday()
+date_of_birth_dtm = dt.datetime(year=1978, month=6, day=5)
+
