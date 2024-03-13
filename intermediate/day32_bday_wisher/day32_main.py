cd2d2b854a71e86917e463dbca923216a775abd2
Nick Hofmeister <5778927+niscient-green@users.noreply.github.com>
2024-03-13 16:10:14 -0400

    Add the extra hard working version of birthday wisher

--- intermediate/day32_bday_wisher/day32_main.py
+++ intermediate/day32_bday_wisher/day32_main.py
@@ -1,26 +1,87 @@
 # Send birthday wishes email
 
-# Import packages
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with
+# the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+
+# Import packages ---------------------------------------------------------------------------------
 import smtplib
 import datetime as dt
+import pandas as pd
+import random
 
-# # Set constants, globals
-# my_email = "testhofmeister@gmail.com"
-# my_password = "rntqyjnhuxvsootu"
-#
-# # Open email connection, send test email
-# with smtplib.SMTP("smtp.gmail.com", port=587) as connection
-#     connection.starttls()
-#     connection.login(user=my_email, password=my_password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs="nick@niscient.com",
-#         msg="Subject: Hi!"
-#             "\n\nJust wishing you a good day.")
+# Set constants, globals --------------------------------------------------------------------------
+MY_EMAIL = "testhofmeister@gmail.com"
+MY_PASSWORD = "rntqyjnhuxvsootu"
+PLACEHOLDER = "[NAME]"
 
-now_dtm = dt.datetime.now()
-year_int = now_dtm.year
-month_int = now_dtm.month
-week_day_str = now_dtm.weekday()
-date_of_birth_dtm = dt.datetime(year=1978, month=6, day=5)
 
+# Handle email ------------------------------------------------------------------------------------
+def send_email(named_letter_lst, match_dct):
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=match_dct["email"],
+            # msg="Subject: Happy birthday!"
+            # f"\n\ntest")
+            msg="Subject: Happy birthday!"
+                f"\n\n{"".join(named_letter_lst)}")
+
+
+# Handle dates ------------------------------------------------------------------------------------
+def get_today():
+    now_dtm = dt.datetime.now()
+    month_int = now_dtm.month
+    day_int = now_dtm.day
+    return month_int, day_int
+
+
+def compare_dates(today_tup, birthdays_dct):
+    match_dct = {}
+    for row in birthdays_dct:
+        if today_tup[0] == birthdays_dct[row]["month"] and today_tup[1] == birthdays_dct[row]["day"]:
+            match_dct = birthdays_dct[row]
+    return match_dct
+
+
+# Handle files  -----------------------------------------------------------------------------------
+def get_letter():
+    which_letter_str = str(random.randint(1, 3))
+    with open("letter_templates/letter_" + which_letter_str + ".txt") as letter_fil:
+        letter_lst = letter_fil.readlines()
+    return letter_lst
+
+
+def get_birthdays():
+    birthdays_dfr = pd.read_csv("birthdays.csv")
+    birthdays_dct = birthdays_dfr.to_dict(orient="index")
+    return birthdays_dct
+
+
+def update_letter(letter_lst, match_dct):
+    updated_letter_lst = letter_lst
+    updated_letter_lst[0] = letter_lst[0].replace(PLACEHOLDER, match_dct["name"])
+    return updated_letter_lst
+
+
+# Main loop  --------------------------------------------------------------------------------------
+
+# Look for birthdays that are today
+today_tup = get_today()
+birthdays_dct = get_birthdays()
+match_dct = compare_dates(today_tup=today_tup, birthdays_dct=birthdays_dct)
+
+# If a matching birthday found, get a random letter, modify it, send it. Otherwise, do nothing.
+if match_dct != {}:
+    letter_lst = get_letter()
+    named_letter_lst = update_letter(letter_lst, match_dct)
+    send_email(named_letter_lst=named_letter_lst, match_dct=match_dct)
+    print("INFO: Sent birthday email.")
+else:
+    print("INFO: Found no matching birthday.")
