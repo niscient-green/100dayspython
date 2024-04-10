40d05c3f92f041f424b9d70d4afb99636217c066
Nick Hofmeister <5778927+niscient-green@users.noreply.github.com>
2024-04-10 19:03:22 -0400

    Add updated version of cookie clicker

--- intermediate/day48_cookie_clicker/day48_main.py
+++ intermediate/day48_cookie_clicker/day48_main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 # Keep Chrome browser open after program finishes
 chrome_options = webdriver.ChromeOptions()
@@ -13,57 +14,92 @@ driver.get("https://orteil.dashnet.org/experiments/cookie/")
 # Main cookie to click
 cookie = driver.find_element(By.ID, value='cookie')
 
+# Get upgrade item ids.
+items = driver.find_elements(by=By.CSS_SELECTOR, value="#store div")
+item_ids = [item.get_attribute("id") for item in items]
+
+# Set timer
+timeout = time.time() + 5
+five_min = time.time() + 60*5  # 5 minutes
+
 # Stats
 cps = driver.find_element(By.ID, value='cps')
-upgrade_elements = None
-upgrade_names = []
+cookie_upgrades = {}
 upgrade_costs = []
 
 
 # Helper functions -----------------------------------------------------------------------------------------------------
 def get_upgrades():
-    # Bring in globals, reset them
-    global upgrade_elements, upgrade_names, upgrade_costs
-    upgrade_names = []
-    upgrade_costs = []
-    upgrade_elements = driver.find_elements(By.CSS_SELECTOR, value='[id^=buy]')
+    global cookie_upgrades, upgrade_costs
 
-    # Parse out the name and cost into lists
-    for n in range(0, len(upgrade_elements) - 1):
-        name = upgrade_elements[n].text.split(' -')[0]
-        cost = int(upgrade_elements[n].text.split('\n')[0].split('- ')[1].replace(',', ''))
-        upgrade_names.append(name)
-        upgrade_costs.append(cost)
+    # Get all upgrade <b> tags
+    all_prices = driver.find_elements(by=By.CSS_SELECTOR, value="#store b")
+    item_prices = []
 
-    # Reverse both lists so the highest cost item is first
-    upgrade_names = upgrade_names.reverse()
-    upgrade_costs = upgrade_costs.reverse()
+    # Convert <b> text into an integer price.
+    for price in all_prices:
+        try:
+            element_text = price.text
+        except:
+            print("Stale")
+        else:
+            if element_text != "":
+                cost = int(element_text.split("-")[1].strip().replace(",", ""))
+                item_prices.append(cost)
+
+    # Create dictionary of store items and prices
+    cookie_upgrades = {}
+    for n in range(len(item_prices)):
+        cookie_upgrades[item_prices[n]] = item_ids[n]
+
+    # Create list of costs for easy sorting
+    upgrade_costs = [cost for cost in cookie_upgrades.items()]
+    upgrade_costs.sort(reverse=True)
+
+    return
 
 
 def get_money():
     # Get total current money
-    money = driver.find_element(By.ID, value='money')
-    return int(money.text)
+    money = int(driver.find_element(By.ID, value='money').text.replace(',',''))
+    return money
 
 
 def buy_upgrades():
+    get_upgrades()
+    money = get_money()
+
     # Buy upgrades, most expensive cost first
-    while True:
+    while money >= upgrade_costs[-1][0]:
+        buy_most_expensive(money)
         get_upgrades()
-        money_int = get_money()
-        for cost in upgrade_costs:
-            if money_int >= cost:
-                
+        money = get_money()
+
+
+def buy_most_expensive(money: int):
+    for upgrade in upgrade_costs:
+        if money >= upgrade[0]:
+            to_purchase_id = upgrade[1]
+            driver.find_element(by=By.ID, value=to_purchase_id).click()
+            return
 
 
 # Main program ---------------------------------------------------------------------------------------------------------
-n = 0
-while n < 20:
+while True:
     cookie.click()
-    n += 1
 
-# buy_upgrades()
-get_upgrades()
+    if time.time() > timeout:
+        buy_upgrades()
+
+        # Add another 5 seconds until the next check
+        timeout = time.time() + 5
+
+    # After 5 minutes stop the bot and check the cookies per second count.
+    if time.time() > five_min:
+        cookie_per_s = driver.find_element(by=By.ID, value="cps").text
+        print(cookie_per_s)
+        break
+
 
 # Close Chrome
 # driver.quit()
